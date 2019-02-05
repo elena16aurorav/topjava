f2a1ae41800d0a40d2bd878417ceda2fb353d010
Elena <aurorav@rambler.ru>
2019-02-06 00:30:53 +0300

    update method with streams

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
+
         List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
@@ -25,16 +26,11 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceededWithStreams(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-
-        Map<LocalDate, Integer> temp = mealList.stream()
-                .collect(Collectors.groupingBy(um -> um.getDateTime().toLocalDate()))
-                .entrySet().stream()
-                .collect(Collectors.toMap(um -> um.getKey(), t -> t.getValue().stream().mapToInt(um -> um.getCalories()).sum()));
-
         return mealList.stream()
                 .filter(um -> (TimeUtil.isBetween(um.getDateTime().toLocalTime(), startTime, endTime)))
                 .map(um -> new UserMealWithExceed(um.getDateTime(), um.getDescription(), um.getCalories(),
-                        temp.get(um.getDateTime().toLocalDate()) > caloriesPerDay ? true : false))
+                        (mealList.stream().collect(Collectors.groupingBy(um1 -> um1.getDateTime().toLocalDate(), Collectors.summingInt(um1 -> um1.getCalories()))))
+                                .get(um.getDateTime().toLocalDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
 
