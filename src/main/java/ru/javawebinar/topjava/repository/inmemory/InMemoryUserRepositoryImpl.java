c348e8d99ff56b5c8b2f589050d88091bb75768f
Elena <aurorav@rambler.ru>
2019-02-20 01:50:43 +0300

    1-2

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepositoryImpl.java
@@ -6,40 +6,64 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepositoryImpl implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
 
+    private Map<Integer, User> users = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
     @Override
     public boolean delete(int id) {
-        log.info("delete {}", id);
-        return true;
+        if(this.users.containsKey(id)){
+            this.users.remove(id);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     @Override
     public User save(User user) {
-        log.info("save {}", user);
-        return user;
+        if(user.isNew()){
+            user.setId(counter.incrementAndGet());
+            this.users.put(user.getId(), user);
+            return user;
+        }
+        else{
+            return users.get(user.getId());
+        }
     }
 
     @Override
     public User get(int id) {
-        log.info("get {}", id);
-        return null;
+        if(this.users.containsKey(id)){
+            return this.users.get(id);
+        }
+        else{
+            return null;
+        }
     }
 
     @Override
     public List<User> getAll() {
-        log.info("getAll");
-        return Collections.emptyList();
+        return (List<User>)this.users.values();
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
+        List<User> lists = getAll();
+        for(User u: lists){
+            if(u.getEmail().equals(email)){
+                return u;
+            }
+        }
         return null;
     }
 }
